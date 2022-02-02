5e5342bfa63ba18528ae4864f44bca2626681770
Marvin Ren <marvin.ren@yale.edu>
2022-02-02 16:05:51 -0500

    resume

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,48 +1,52 @@
 import React from 'react'
-// import images from '../../assets/images'
-
+import simpleDoc from '../../assets/images/simple-doc.jpg'
+import eat from '../../assets/images/eat-in-peace.jpg'
+import weather from '../../assets/images/weather-dashboard.jpg'
+import team from '../../assets/images/team-generator.jpg'
+import run from '../../assets/images/run-buddy.jpg'
+import lyza from '../../assets/images/lyza-portfolio.jpg'
 
 function PhotoList() {
 
     const photos = [
         {
             title: 'Simple Doc',
-            filename: 'simple-doc',
-            description: 'Manages a small hospital.',
+            filename: simpleDoc,
+            description: 'Helps manages a small hospital.',
             github: 'https://github.com/mr2447/simple-doc',
             deploy: 'https://enigmatic-sea-73658.herokuapp.com/'
         },
         {
             title: 'Eat in Peace',
-            filename: 'eat-in-peace',
+            filename: eat,
             description: "Track user's diet based on BMI.",
             github: 'https://github.com/mr2447/dreamteam',
             deploy: 'https://mr2447.github.io/dreamteam/'
         },
         {
             title: 'Weather Dashboard',
-            filename: 'weather-dashboard',
+            filename: weather,
             description: 'Checks weather with third party API.',
             github: 'https://github.com/mr2447/weather-daashboard',
             deploy: 'https://github.com/mr2447/weather-daashboard'
         },
         {
             title: 'Team Generator',
-            filename: 'team-generator',
+            filename: team,
             description: 'Generates team profile.',
             github: 'https://github.com/mr2447/team-profile-generator',
             deploy: 'https://github.com/mr2447/team-profile-generator'
         },
         {
             title: 'Run Buddy',
-            filename:'run-buddy',
+            filename:run,
             description: 'A front-end html for small business.',
             github: 'https://mr2447.github.io/run-buddy/',
             deploy: 'https://mr2447.github.io/run-buddy/'
         },
         {
             title: "Lyza's Portfolio",
-            filename:'lyza-portfolio',
+            filename: lyza,
             description: "Lyza's photography portfolio.",
             github: 'https://github.com/mr2447/lyza-portfolio',
             deploy: 'https://mr2447.github.io/lyza-portfolio/' 
@@ -52,10 +56,10 @@ function PhotoList() {
         <div className="flex-row">
             {photos.map((photo)=> { 
                 return (
-                    <div key={photo.filename}>
+                    <div className="image-container" key={photo.filename}>
                     <a href={photo.deploy} target="_blank">
                     <img
-                    src={`images/${photos.filename}.jpg`}
+                    src={photo.filename}
                     alt={photo.title}
                     className="img-thumbnail mx-1"
                     />
