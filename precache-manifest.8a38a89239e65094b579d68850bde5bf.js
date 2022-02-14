9c3c3544a89725f5a34d2313237ad4aa5d3131f5
Marvin Ren <marvin.ren@yale.edu>
2022-02-14 13:06:46 -0500

    Updates

---
+++ precache-manifest.8a38a89239e65094b579d68850bde5bf.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1502c375e723c233af481077d1e504a1",
+    "url": "/marvin-portfolio/index.html"
+  },
+  {
+    "revision": "cd4133febd4520b1b8d8",
+    "url": "/marvin-portfolio/static/css/main.1d33e182.chunk.css"
+  },
+  {
+    "revision": "6f7475b47612a529e906",
+    "url": "/marvin-portfolio/static/js/2.5f9e0370.chunk.js"
+  },
+  {
+    "revision": "5f5a2437a5d1efa8c90937d119e5d662",
+    "url": "/marvin-portfolio/static/js/2.5f9e0370.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d85a4073daf15049154c",
+    "url": "/marvin-portfolio/static/js/3.49435f5c.chunk.js"
+  },
+  {
+    "revision": "cd4133febd4520b1b8d8",
+    "url": "/marvin-portfolio/static/js/main.b68e3cfc.chunk.js"
+  },
+  {
+    "revision": "10cf8256229f76b90801",
+    "url": "/marvin-portfolio/static/js/runtime-main.c176f3e1.js"
+  },
+  {
+    "revision": "141cd5f088c7a1204c0a496453e3e8aa",
+    "url": "/marvin-portfolio/static/media/coding-resume.141cd5f0.pdf"
+  },
+  {
+    "revision": "6d28536313d885b81244805b7eb9ded6",
+    "url": "/marvin-portfolio/static/media/eat-in-peace-poster.6d285363.jpg"
+  },
+  {
+    "revision": "d084c9d4c942097546ff111f8f11e132",
+    "url": "/marvin-portfolio/static/media/eat-in-peace.d084c9d4.jpg"
+  },
+  {
+    "revision": "f71268ecb9fe0aaa00715426123f9013",
+    "url": "/marvin-portfolio/static/media/forest-img.f71268ec.jpg"
+  },
+  {
+    "revision": "f3f1cec1c15fc39adcf01d0c61155cf5",
+    "url": "/marvin-portfolio/static/media/guitar-shop-poster.f3f1cec1.jpg"
+  },
+  {
+    "revision": "f1dd60f737cb7635d9d1c08c71dc6f45",
+    "url": "/marvin-portfolio/static/media/guitar-shop.f1dd60f7.jpg"
+  },
+  {
+    "revision": "dea038ead6f75638dd01fadf7b2fdb5c",
+    "url": "/marvin-portfolio/static/media/lyza-portfolio.dea038ea.jpg"
+  },
+  {
+    "revision": "ae7438e7450531a039f2571d91c15b3d",
+    "url": "/marvin-portfolio/static/media/me-coding.ae7438e7.jpg"
+  },
+  {
+    "revision": "4469f2c64f345602cf62160471b53fa9",
+    "url": "/marvin-portfolio/static/media/run-buddy.4469f2c6.jpg"
+  },
+  {
+    "revision": "9ec0f241bca69774e210caa372e1f9c4",
+    "url": "/marvin-portfolio/static/media/self-image.9ec0f241.jpg"
+  },
+  {
+    "revision": "7b67e291927d22de168b87de57552856",
+    "url": "/marvin-portfolio/static/media/simple-doc-poster.7b67e291.jpg"
+  },
+  {
+    "revision": "66ace83673a1d44b035f2d24d0e46d11",
+    "url": "/marvin-portfolio/static/media/simple-doc.66ace836.jpg"
+  },
+  {
+    "revision": "66c9f3fb25cc55ef1af458023efcf8aa",
+    "url": "/marvin-portfolio/static/media/team-generator.66c9f3fb.jpg"
+  },
+  {
+    "revision": "42e2c479d17af13e8a435431358472c3",
+    "url": "/marvin-portfolio/static/media/weather-dashboard.42e2c479.jpg"
+  }
+]);
